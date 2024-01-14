7c8bf88e1a997f90621c1d7ab9bc9a8ffad9a938
Omokhefue <mokhefue@gmail.com>
2024-01-15 00:11:52 +0100

    added home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
+import React from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <>
-      <p>T&T Bookings</p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
