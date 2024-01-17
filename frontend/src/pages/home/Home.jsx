9999f55a55176984b5f8bc481bdc53b08a497e0d
Omokhefue <mokhefue@gmail.com>
2024-01-17 08:37:39 +0100

    imported top destination component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import TravelTips from "../../components/travelTips/TravelTips";
+import HomepageTopDestination from "../../components/HomepageTopDestination/HomepageTopDestination";
 
 const Home = () => {
   return (
     <div>
+      <HomepageTopDestination />
       <TravelTips />
-
     </div>
   );
 };
