0123314495d02079a69846257b72c5bba88fee66
Omokhefue <mokhefue@gmail.com>
2024-01-17 08:51:28 +0100

    adjusted width

--- frontend/src/components/travelTips/TravelTips.jsx
+++ frontend/src/components/travelTips/TravelTips.jsx
@@ -7,15 +7,15 @@ const TravelTips = () => {
   const randomIndex = Math.floor(Math.random() * tips.length);
   const tip = tips[randomIndex];
   return (
-    <div className="travel-tips-container">
-      {/* <div className="a"> */}
-        {/* <h2 className="travel-tips-heading">Travel Tips</h2> */}
+    <section className=" section travel-tips">
+      <div className="travel-tips-container">
+        <h2>Travel Tips</h2>
         <div className="tip-card">
           <h3 className="tip-title">{tip.title}</h3>
           <p className="tip-description">{tip.description}</p>
         </div>
-      {/* </div> */}
-    </div>
+      </div>
+    </section>
   );
 };
 export default TravelTips;
