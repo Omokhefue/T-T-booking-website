57ba53b83e7fbfe8654ef192e71f8bbf1b160eba
Omokhefue <mokhefue@gmail.com>
2024-01-17 08:32:56 +0100

    top destination component for homepage

---
+++ frontend/src/components/homepageTopDestination/HomepageTopDestination.jsx
@@ -0,0 +1,80 @@
+import "./homepageTopDestination.css";
+
+const HomepageTopDestination = () => {
+  return (
+    <section className="section destinationContainer">
+      <h2 className="section-heading">Top Destination</h2>
+      <span className="section-span destinationContainerSpan"></span>
+
+      <div className="destinationList">
+        <div className="destinationListItem">
+          <img src="Monsieur-de-North.jpg" alt="" />
+          <div className="destinationDetails">
+            <h5>France</h5>
+            <span className="section-span destinationDetailsSpan"></span>
+            <p>20 hotels</p>
+            <p>42 apartments</p>
+            <p>36 cabins</p>
+            <p></p>
+          </div>
+        </div>
+        <div className="destinationListItem">
+          <img src="Monsieur-de-North.jpg" alt="" />
+          <div className="destinationDetails">
+            <h5>France</h5>
+            <span className="section-span destinationDetailsSpan"></span>
+            <p>20 hotels</p>
+            <p>42 apartments</p>
+            <p>36 cabins</p>
+            <p></p>
+          </div>
+        </div>
+        <div className="destinationListItem">
+          <img src="Monsieur-de-North.jpg" alt="" />
+          <div className="destinationDetails">
+            <h5>France</h5>
+            <span className="section-span destinationDetailsSpan"></span>
+            <p>20 hotels</p>
+            <p>42 apartments</p>
+            <p>36 cabins</p>
+            <p></p>
+          </div>
+        </div>
+        <div className="destinationListItem">
+          <img src="Monsieur-de-North.jpg" alt="" />
+          <div className="destinationDetails">
+            <h5>France</h5>
+            <span className="section-span destinationDetailsSpan"></span>
+            <p>20 hotels</p>
+            <p>42 apartments</p>
+            <p>36 cabins</p>
+            <p></p>
+          </div>
+        </div>
+        <div className="destinationListItem">
+          <img src="Monsieur-de-North.jpg" alt="" />
+          <div className="destinationDetails">
+            <h5>France</h5>
+            <span className="section-span destinationDetailsSpan"></span>
+            <p>20 hotels</p>
+            <p>42 apartments</p>
+            <p>36 cabins</p>
+            <p></p>
+          </div>
+        </div>
+        <div className="destinationListItem">
+          <img src="Monsieur-de-North.jpg" alt="" />
+          <div className="destinationDetails">
+            <h5>France</h5>
+            <span className="section-span destinationDetailsSpan"></span>
+            <p>20 hotels</p>
+            <p>42 apartments</p>
+            <p>36 cabins</p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default HomepageTopDestination;
