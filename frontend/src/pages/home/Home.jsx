9391cc3fe9cc09c4fa5b632d68224369cbeb3c0b
Omokhefue <mokhefue@gmail.com>
2024-01-18 21:41:23 +0100

    added featured component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import TravelTips from "../../components/travelTips/TravelTips";
 import HomepageTopDestination from "../../components/homepageTopDestination/HomepageTopDestination.jsx";
+import Choose from "../../components/choose/Choose.jsx";
+import PropertyType from "../../components/propertyType/propertyType.jsx";
+import Featured from "../../components/featured/Featured.jsx";
 
 const Home = () => {
   return (
     <div>
       <HomepageTopDestination />
+      <Choose />
+      <PropertyType />
+      <Featured />
       <TravelTips />
     </div>
   );
