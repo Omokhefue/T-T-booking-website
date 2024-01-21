16cc4d35f6b7448b53b9c95aaa771e5936e14679
Omokhefue <mokhefue@gmail.com>
2024-01-21 12:12:00 +0100

    conditional display of components and context for form dispslay

--- frontend/src/pages/search/Search.jsx
+++ frontend/src/pages/search/Search.jsx
@@ -2,12 +2,43 @@ import SearchForm from "../../components/search/SearchForm";
 import SearchResult from "../../components/searchResult/SearchResult";
 import useWindowDimensions from "../../hooks/useWindowDimension.jsx";
 import "./search.css";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SearchPageContext } from "../../context/SearchPageContext.jsx";
+import { useContext, useEffect, useState } from "react";
+
 const Search = () => {
+  const { state, dispatch } = useContext(SearchPageContext);
   const { width } = useWindowDimensions();
+
+  const [display, setDisplay] = useState(state.formDisplay);
+
+  const handleClick = () => {
+    dispatch({
+      type: "FORM_OPEN",
+    });
+  };
+  useEffect(() => {
+    if (width > 576) {
+      dispatch({ type: "FORM_OPEN" });
+    } else {
+      dispatch({ type: "FORM_CLOSE" });
+    }
+  }, [width]);
+  useEffect(() => {
+
+    setDisplay(state.formDisplay);
+  }, [state.formDisplay]);
+
   return (
     <section className="search">
-      {width  && <SearchForm />}
+      {display && <SearchForm />}
       <SearchResult />
+      {!display && (
+        <div className="searchIcon" onClick={handleClick}>
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+      )}
     </section>
   );
 };
