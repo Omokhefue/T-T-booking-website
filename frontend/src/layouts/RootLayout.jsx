27355fe4fdb69117f012832ec913ad9ec97d8b20
Omokhefue <mokhefue@gmail.com>
2024-01-22 11:07:07 +0100

    conditional header rendering

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
 
 const RootLayout = () => {
   const location = useLocation();
   return (
     <>
       <Navbar />
+
+      {location.pathname === ("/" || "/search") && <Header />}
       <Outlet />
       <Footer />
     </>
