c5557ebfd63d194b06f752c3234c46b2f4c84185
Omokhefue <mokhefue@gmail.com>
2024-01-24 22:47:12 +0100

    added singkw room page import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,15 @@ import {
 } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import Room from "./pages/room/Room";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-
       <Route index element={<Home />} />
+      <Route path="search" element={<Search />} />
+      <Route path="rooms/room" element={<Room />} />
     </Route>
   )
 );
