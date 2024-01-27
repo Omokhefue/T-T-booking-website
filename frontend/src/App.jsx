d54a18de368850987e16689af25f51b13be501f2
Omokhefue <mokhefue@gmail.com>
2024-01-27 11:30:21 +0100

    routes for signup and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,18 +13,24 @@ import Register from "./components/register/Register";
 import Apartments from "./pages/apartment/Apartments";
 import Hotels from "./pages/hotel/Hotels";
 import Cabins from "./pages/cabin/Cabins";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="register" element={<Register />} />
-      <Route path="search" element={<Search />} />
-      <Route path="rooms/room" element={<Room />} />
-      <Route path="apartments" element={<Apartments />} />
-      <Route path="hotels" element={<Hotels />} />
-      <Route path="cabins" element={<Cabins />} />
-    </Route>
+    <>
+      <Route path="signup" element={<Signup />} />
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="search" element={<Search />} />
+        <Route path="rooms/room" element={<Room />} />
+        <Route path="apartments" element={<Apartments />} />
+        <Route path="hotels" element={<Hotels />} />
+        <Route path="cabins" element={<Cabins />} />
+      </Route>
+    </>
   )
 );
 
