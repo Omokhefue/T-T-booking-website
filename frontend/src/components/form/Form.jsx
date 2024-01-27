18ddbb25824aa656552a6530ea7258b31d4856c3
Omokhefue <mokhefue@gmail.com>
2024-01-27 11:31:09 +0100

    component for form section

---
+++ frontend/src/components/form/Form.jsx
@@ -0,0 +1,38 @@
+import "./form.css";
+
+const Form = ({
+  formType,
+  heading,
+  fields,
+  buttonText,
+  extraElements,
+  img,
+}) => {
+  return (
+    <div className="section-form">
+      <div className="imageContainer">
+        <img src={img} alt="" />
+      </div>
+      <div className="form-container">
+        <h2>{heading}</h2>
+        <form>
+          {fields.map((field) => (
+            <div key={field.id} className="form-group">
+              <label htmlFor={field.id}>{field.label}</label>
+              <input
+                type={field.type}
+                id={field.id}
+                name={field.name}
+                placeholder={field.placeholder}
+              />
+            </div>
+          ))}
+          <button type="submit">{buttonText}</button>
+        </form>
+      </div>
+        {extraElements && extraElements}
+    </div>
+  );
+};
+
+export default Form;
