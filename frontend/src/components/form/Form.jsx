b03953528e36e811063419c3c626e63d6b1b287c
Omokhefue <mokhefue@gmail.com>
2024-01-28 11:18:51 +0100

    div

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -13,24 +13,26 @@ const Form = ({
       <div className="imageContainer">
         <img src={img} alt="" />
       </div>
-      <div className="form-container">
-        <h2>{heading}</h2>
-        <form>
-          {fields.map((field) => (
-            <div key={field.id} className="form-group">
-              <label htmlFor={field.id}>{field.label}</label>
-              <input
-                type={field.type}
-                id={field.id}
-                name={field.name}
-                placeholder={field.placeholder}
-              />
-            </div>
-          ))}
-          <button type="submit">{buttonText}</button>
-        </form>
-      </div>
+      <div className="details">
+        <div className="form-container">
+          <h2>{heading}</h2>
+          <form>
+            {fields.map((field) => (
+              <div key={field.id} className="form-group">
+                <label htmlFor={field.id}>{field.label}</label>
+                <input
+                  type={field.type}
+                  id={field.id}
+                  name={field.name}
+                  placeholder={field.placeholder}
+                />
+              </div>
+            ))}
+            <button type="submit">{buttonText}</button>
+          </form>
+        </div>
         {extraElements && extraElements}
+      </div>
     </div>
   );
 };
