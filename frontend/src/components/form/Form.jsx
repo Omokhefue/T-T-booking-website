a22d2647101f2f5a57e6afcc3c574305acdadb73
Omokhefue <mokhefue@gmail.com>
2024-01-29 22:40:28 +0100

    div for styling big screens

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -1,38 +1,70 @@
 import "./form.css";
+import useWindowDimensions from "../../hooks/useWindowDimension.jsx";
+
 
 const Form = ({
-  formType,
   heading,
   fields,
   buttonText,
   extraElements,
   img,
 }) => {
+  const { width } = useWindowDimensions();
   return (
     <div className="section-form">
-      <div className="imageContainer">
-        <img src={img} alt="" />
-      </div>
-      <div className="details">
-        <div className="form-container">
-          <h2>{heading}</h2>
-          <form>
-            {fields.map((field) => (
-              <div key={field.id} className="form-group">
-                <label htmlFor={field.id}>{field.label}</label>
-                <input
-                  type={field.type}
-                  id={field.id}
-                  name={field.name}
-                  placeholder={field.placeholder}
-                />
-              </div>
-            ))}
-            <button type="submit">{buttonText}</button>
-          </form>
+      {width >= 1286 ? (
+        <div className="big-screen">
+          <div className="imageContainer">
+            <img src={img} alt="" />
+          </div>
+          <div className="details">
+            <div className="form-container">
+              <h2>{heading}</h2>
+              <form>
+                {fields.map((field) => (
+                  <div key={field.id} className="form-group">
+                    <label htmlFor={field.id}>{field.label}</label>
+                    <input
+                      type={field.type}
+                      id={field.id}
+                      name={field.name}
+                      placeholder={field.placeholder}
+                    />
+                  </div>
+                ))}
+                <button type="submit">{buttonText}</button>
+              </form>
+            </div>
+            {extraElements && extraElements}
+          </div>
         </div>
-        {extraElements && extraElements}
-      </div>
+      ) : (
+        <>
+          <div className="imageContainer">
+            <img src={img} alt="" />
+          </div>
+          <div className="details">
+            <div className="form-container">
+              <h2>{heading}</h2>
+              <form>
+                {fields.map((field) => (
+                  <div key={field.id} className="form-group">
+                    <label htmlFor={field.id}>{field.label}</label>
+                    <input
+                      type={field.type}
+                      id={field.id}
+                      name={field.name}
+                      placeholder={field.placeholder}
+                    />
+                  </div>
+                ))}
+                <button type="submit">{buttonText}</button>
+              </form>
+            </div>
+            {extraElements && extraElements}
+          </div>
+        </>
+      )}
     </div>
   );
 };
