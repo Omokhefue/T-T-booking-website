f8ce03eac21b2914bc551245db35adad4f26ec02
Omokhefue <mokhefue@gmail.com>
2024-02-04 13:03:36 +0100

    auth context provider import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import SearchContextProvider from "./context/SearchContext.jsx";
+import SearchPageContextProvider from "./context/SearchPageContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthContextProvider>
+    <SearchContextProvider>
+      <SearchPageContextProvider>
+        <App />
+      </SearchPageContextProvider>
+    </SearchContextProvider>
+  </AuthContextProvider>
+);
